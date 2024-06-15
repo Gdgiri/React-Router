c7e9b2a1224f5bb8846c7ed60f3175be0aab7e5f
Gdgiri <giridharan831@gmail.com>
2024-06-15 17:15:33 +0530

    update

--- src/Pages/Pagenotfound.jsx
+++ src/Pages/Pagenotfound.jsx
@@ -1,17 +1,58 @@
-import React from "react";
+// NotFoundPage.js
 
-const Pagenotfound = () => {
+import React, { useEffect } from "react";
+
+const NotFoundPage = () => {
+  useEffect(() => {
+    const body = document.body;
+    const createStar = () => {
+      const right = Math.random() * 500;
+      const top = Math.random() * window.innerHeight;
+      const star = document.createElement("div");
+      star.classList.add("star");
+      body.appendChild(star);
+      let rightPosition = right;
+      const runStar = () => {
+        if (rightPosition >= window.innerWidth) {
+          star.remove();
+        }
+        rightPosition += 3;
+        star.style.right = rightPosition + "px";
+      };
+      star.style.top = top + "px";
+      setInterval(runStar, 10);
+    };
+
+    const intervalId = setInterval(createStar, 100);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div>
-      <img
-        src="https://c.tenor.com/06KDMj1RfgQAAAAC/tenor.gif"
-        alt="404-notfound"
-        width={300}
-        className="p-5 m-5 d-flex alerts"
-      />
-      <h1 className="bg-danger p-5 m-5 text-white">404-Page Not Found</h1>
+    <div className="not-found-page">
+      <div className="center">
+        <section className="error">
+          <h1 className="error__title">404</h1>
+          <h2 className="error__type">Page not found</h2>
+          <p className="error__cta">
+            We’re sorry, the page you have looked for does not exist
+            <a
+              className="error__link error__link--purple"
+              href="/"
+              target="_blank"
+            >
+              home page
+            </a>
+          </p>
+        </section>
+      </div>
+      <div className="astronaut">
+        <img
+          src="https://images.vexels.com/media/users/3/152639/isolated/preview/506b575739e90613428cdb399175e2c8-space-astronaut-cartoon-by-vexels.png"
+          alt="Astronaut"
+        />
+      </div>
     </div>
   );
 };
 
-export default Pagenotfound;
+export default NotFoundPage;
